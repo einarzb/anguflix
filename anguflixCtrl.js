13e9a9ed73a17106dbcaaf0f49a8b4c2076ba798
einarzb <einarzb@gmail.com>
2017-03-05 12:26:33 +0200

    ng-hide to h3 if collection is empty

--- anguflixCtrl.js
+++ anguflixCtrl.js
@@ -1,7 +1,11 @@
 app.controller('anguflixCtrl', function($scope, anguflixService) {
+
+  window.MY_SCOPE = $scope;
+
   $scope.moviesList = anguflixService.moviesList;
   $scope.userBudget = 100; //initialize budget!
   $scope.userCollection = anguflixService.userCollection;
+  $scope.emptyCollection = false; //show h3
 
   //most importent function to get API 
   $scope.searchMovie = anguflixService.searchMovie;
@@ -11,23 +15,25 @@ app.controller('anguflixCtrl', function($scope, anguflixService) {
   $scope.popular = anguflixService.popular;
   
   $scope.addToCollection = function(newMovie){
-    //scaning the array
-    for(var i = 0; i < anguflixService.userCollection.length; i++) {
+    //scanning the array
+  for(var i = 0; i < anguflixService.userCollection.length; i++) {
     //checks if movie exists in array
       if(newMovie === anguflixService.userCollection[i]) {
-        alert("movie is alreay in list");
+        alert("movie is already in list");
         return;
     //if movie exists than alert and abort    
       } 
     };
+
     //the else happens OUTSIDE the loop and only THAN adds the new movie to array 
+        $scope.emptyCollection = true; //hide h3
         anguflixService.addToCollection(newMovie); //invoke service function 
         console.log($scope.userCollection);
     };
    
   $scope.removeFromList = function(index){
-    anguflixService.removeFromList(index);
-    console.log($scope.userCollection);
+      anguflixService.removeFromList(index);
+      console.log($scope.userCollection);
   }
 
 
