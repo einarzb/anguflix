ded5334042d3db0cc4db7b64169b9910dc260c63
einarzb <einarzb@gmail.com>
2017-03-05 16:02:05 +0200

    added   <section ng-controller=anguflixCtrl> and also toggled the emptycollection h4

--- anguflixCtrl.js
+++ anguflixCtrl.js
@@ -1,11 +1,9 @@
 app.controller('anguflixCtrl', function($scope, anguflixService) {
 
-  window.MY_SCOPE = $scope;
-
   $scope.moviesList = anguflixService.moviesList;
   $scope.userBudget = 100; //initialize budget!
   $scope.userCollection = anguflixService.userCollection;
-  $scope.emptyCollection = false; //show h3
+  $scope.emptyCollection = true; //show h3
 
   //most importent function to get API 
   $scope.searchMovie = anguflixService.searchMovie;
@@ -16,7 +14,7 @@ app.controller('anguflixCtrl', function($scope, anguflixService) {
   
   $scope.addToCollection = function(newMovie){
     //scanning the array
-  for(var i = 0; i < anguflixService.userCollection.length; i++) {
+    for(var i = 0; i < anguflixService.userCollection.length; i++) {
     //checks if movie exists in array
       if(newMovie === anguflixService.userCollection[i]) {
         alert("movie is already in list");
@@ -26,14 +24,26 @@ app.controller('anguflixCtrl', function($scope, anguflixService) {
     };
 
     //the else happens OUTSIDE the loop and only THAN adds the new movie to array 
-        $scope.emptyCollection = true; //hide h3
-        anguflixService.addToCollection(newMovie); //invoke service function 
-        console.log($scope.userCollection);
+    if($scope.emptyCollection){
+      $scope.emptyCollection = false; //hide h3
+    }else{
+      $scope.emptyCollection = true;
+    }
+
+    anguflixService.addToCollection(newMovie); //invoke service function 
+    console.log($scope.userCollection);
     };
    
   $scope.removeFromList = function(index){
       anguflixService.removeFromList(index);
       console.log($scope.userCollection);
+
+      if($scope.emptyCollection){
+        $scope.emptyCollection = false; //hide h3
+      }else{
+        $scope.emptyCollection = true;
+      }
+
   }
 
 
