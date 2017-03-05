744b182fde20dd3223212fd138a034b519cf1c05
einarzb <einarzb@gmail.com>
2017-03-05 11:45:43 +0200

    added double check if else to see

--- anguflixCtrl.js
+++ anguflixCtrl.js
@@ -11,11 +11,18 @@ app.controller('anguflixCtrl', function($scope, anguflixService) {
   $scope.popular = anguflixService.popular;
   
   $scope.addToCollection = function(newMovie){
-    if (newMovie === anguflixService.userCollection) {
-         alert("movie is alreay in list");
-    }else{     
+    //scaning the array
+    for(var i = 0; i < anguflixService.userCollection.length; i++) {
+    //checks if movie exists in array
+      if(newMovie === anguflixService.userCollection[i]) {
+        alert("movie is alreay in list");
+        return;
+    //if movie exists than alert and abort    
+      } 
+    };
+    //the else happens OUTSIDE the loop and only THAN adds the new movie to array 
         anguflixService.addToCollection(newMovie); //invoke service function 
-        console.log($scope.userCollection);} 
+        console.log($scope.userCollection);
     };
    
   $scope.removeFromList = function(index){
